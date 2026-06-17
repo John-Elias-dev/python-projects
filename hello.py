8d94846e8f3ebcfff0ac2bebed5d504ebe9bb23a
John Elias <omaniajohnelias@gmail.com>
2026-06-17 19:27:34 +0300

    Add GitBash test print

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print("Hello World frm windows 11")
+print("Hello John! I beat GitHub today")
+print(10+25)
+name=input("What's your name? ")
+print("Nice to meet you,"+name+"!")
+age=input("How old are you? ") 
+age=int(age) # Convert text to number first
+
+if age >=18:
+    print("You're an adult! You can vote")
+else:
+    print("You're still a kid! Enjoy the freedom")
+    print("GitBash works")
